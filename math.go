4078aae513e994b08363fd6fb1f45f2679efdea2
Felipe Araujo <devaraujofelipe@gmail.com>
2021-05-11 16:19:24 -0300

    feat: :sparkles: unpublish sum function
    BREAKING: other packages won't be able to use the sum function

--- math.go
+++ math.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(Sum(10, 10))
+	fmt.Println(sum(10, 10))
 }
 
-func Sum(firstNumber int, secondNumber int) int {
+func sum(firstNumber int, secondNumber int) int {
 	return firstNumber + secondNumber
 }
